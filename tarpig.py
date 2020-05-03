37c00401a67cbc9b1f319cc336354a2bbdd51561
Younis Latyp <younislatyp@gmail.com>
2020-05-02 21:08:23 -0400

    Finishing the weapons

--- tarpig.py
+++ tarpig.py
@@ -11,7 +11,7 @@ class Loc:
     def __init__(self, cord, name, objs):
         self.cord = cord
         self.name = name
-        self.objs = []
+        self.objs = objs
 
 
 class Player:
@@ -37,6 +37,9 @@ class Player:
         self.hp = newHP
         if self.hp > self.mhp:
             self.hp = self.mhp
+        if self.hp <= 0:
+            self.living = False
+            self.hp = 0
 
     def heal(self, heal):
         if isinstance(heal, Heal):
@@ -50,9 +53,7 @@ class Player:
             print(f"{heal.name} is not a heal.")
     
     def attack(self, target):
-        target.setHP -= holding.dmg
-        if target.hp < 1:
-            target.living = False
+        target.setHP(target.hp-self.holding.dmg)
 
     def getinv(self):
         for x in self.inv:
@@ -82,12 +83,14 @@ class Weapon(Item):
 
 sys.stdout.write("\033[2J\033[H")
 print("TaRPiG - Text Roleplaying game")
-
-hmm = Player(5, 10, ["car", "apple", "banana"], True, "car", "apple")
-print("You are in a field")
+ayy = Weapon("ayy", "", "h", 5)
+hmm = Player(5, 10, [ayy, "apple", "banana"], True, ayy, "apple")
+smh = Player(5, 10, [ayy, "apple", "banana"], True, ayy, "apple")
 awesom = Heal("poop", "some poop", "some guy", 5)
-#hmm.addinv(awesom)
+
 hmm.health()
-hmm.heal(awesom)
-print(isinstance(awesom, Item))
-a = input("hmm")
+smh.attack(hmm)
+hmm.health()
+print("Is hmm living?", hmm.living)
+print("You are standing in a field")
+input("hmm")
